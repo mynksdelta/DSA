399adb2250961783f687152ef7693f161c4ec8a8
Mayank Sharma <mayankmssharma18@gmail.com>
2022-03-10 22:46:49 +0530

    pointer program and commit practice

--- pointer.cpp
+++ pointer.cpp
@@ -1,8 +1,26 @@
 #include<iostream>
+#include<stdio.h>
 using namespace std;
 
 int main()
 {
-    cout << "hello";
+    // Pointer to a variable
+    int a = 10;
+
+    // Use * only for initialisation and derefrencing
+    int *p;
+
+    // Address of a is stored in the pointer p
+    p = &a;
+
+    // Printing the value of a
+    cout << a << endl;
+
+    // Printing the value of p that's pointing to a
+    printf("Address of a is: %d \n", p);
+
+    // Printing the value of a using p
+    printf("Derefrencing: %d \n", *p);
+
     return 0;
 }
