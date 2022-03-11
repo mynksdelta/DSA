5506f2a02488592888f6e40b43c7c92c549948ec
Mayank Sharma <mayankmssharma18@gmail.com>
2022-03-11 10:25:57 +0530

    array in heap added

--- pointer_to_an_array.cpp
+++ pointer_to_an_array.cpp
@@ -8,8 +8,18 @@ int main()
     int A[3] = {2, 1, 5};
 
     // Creating a pointer variable p
-    int *p;
+    int *p, *h;
 
+    // Creating array in heap memory, type casting the array into int using malloc function
+    h = (int *)malloc(5 * sizeof(int));
+
+    // Defining the values of array in heap
+    h[0] = 11;
+    h[1] = 3;
+    h[2] = 23;
+    h[3] = 15;
+    h[4] = 2;
+    
     // Storing the address of A into p
     // A also denoted the starting addrss of the array so no need to use &
     p = A;
@@ -17,6 +27,11 @@ int main()
     // Using for loop to print all values of A using pointer p
     for(int i = 0; i < 3; i++)
     cout << p[i] << endl;
-    
+
+    // Printing the values of array stored in heap
+    cout << "----------------" << endl;
+    for(int j = 0; j < 5; j++)
+    cout << h[j] << endl;
+
     return 0;
 }
