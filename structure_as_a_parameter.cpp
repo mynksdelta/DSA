c6457dadfa585ac0fb359d46691674f2b53f7aa5
Mayank Sharma <mayankmssharma18@gmail.com>
2022-03-22 12:28:45 +0530

    Added comments

--- structure_as_a_parameter.cpp
+++ structure_as_a_parameter.cpp
@@ -2,11 +2,14 @@
 #include<stdio.h>
 using namespace std;
 
+// Creating a struct named rectangle
 struct Rectangle {
     int length;
     int breadth;
 };
 
+// Function area that returns the area of struct
+// For pass by value use 'r' and for pass by reference use '&r'
 int area(struct Rectangle r)
 {
     return r.length * r.breadth;
@@ -14,6 +17,7 @@ int area(struct Rectangle r)
 
 int main()
 {
+    // Defining the struct
     struct Rectangle r = {2, 5};
     printf("%d",  area(r));
     return 0;
