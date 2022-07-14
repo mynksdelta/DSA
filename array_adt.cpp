c6d788d6e31e07806df341f8fa86991f33470914
Mayank Sharma <mayankmssharma18@gmail.com>
2022-07-15 01:13:28 +0530

    array_adt.cpp added

---
+++ array_adt.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+struct Array
+{
+    int *A;
+    int size;
+    int length;
+};
+
+void Display(struct Array arr)
+{
+    int i;
+    printf("\n Elements are \n");
+
+    for(i = 0; i < arr.length; i++)
+    {
+        printf("%d", arr.A[i]);
+    }
+}
+
+int main()
+{
+    struct Array arr;
+    int n, i;
+    printf("Enter size of an array: ");
+    scanf("%d", &arr.size);
+    arr.A = (int *)malloc(arr.size*sizeof(int));
+    arr.length = 0;
+
+    printf("Enter number of numbers: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr.A[i]);
+    }
+    arr.length = n;
+
+    Display(arr);
+
+    return 0;
+}
